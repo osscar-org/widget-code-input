ab2b3bd55d4f3e507c095469fe1189e8f79af4f5
Dou Du <dou.du@epfl.ch>
2022-10-30 12:12:01 +0100

    make PyPi version number 0.3.5

--- widget_code_input/_version.py
+++ widget_code_input/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Dou Du.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 0, 'dev')
+version_info = (3, 5, 0, 'dev')
 __version__ = ".".join(map(str, version_info))
